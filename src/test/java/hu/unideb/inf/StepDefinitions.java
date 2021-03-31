47eda9c246a241299c7a0e35cfd616f5ceda42d5
ShifraSec <48570596+MoElaSec@users.noreply.github.com>
2021-03-31 08:11:41 +0200

    Initial commit

---
+++ src/test/java/hu/unideb/inf/StepDefinitions.java
@@ -0,0 +1,67 @@
+package hu.unideb.inf;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
+
+public class StepDefinitions {
+
+    private static final int WAIT_TIME = 10;
+
+    static WebDriver driver;
+
+    private HomePage homePage;
+
+    static {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        //options.setHeadless(true);
+        //options.addArguments("--no-sandbox");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(WAIT_TIME, TimeUnit.SECONDS);
+    }
+
+    @Given("the home page is opened")
+    public void theHomePageIsOpened() {
+        homePage = new HomePage(driver);
+    }
+
+    @And("the Sign In link is clicked")
+    public void theSignInLinkIsClicked() {
+        homePage.clickSignInLink();
+    }
+
+    @Given("the Sign In button is clicked")
+    public void theSignInButtonIsClicked() {
+        homePage.clickSignInButton();
+    }
+
+    @Given("The {string} is filled with {string}")
+    public void theFieldIsFilledWithParameter(String field, String parameter) {
+        homePage.fillField(field, parameter);
+    }
+
+    @Then("The {string} error message is shown")
+    public void theMsgErrorMessageIsShown(String msg) {
+        Optional<String> errorMessage = homePage.getErrorMessage();
+        if (errorMessage.isPresent()) {
+            Assert.assertEquals(msg, errorMessage.get());
+        } else {
+            fail();
+        }
+    }
+}
